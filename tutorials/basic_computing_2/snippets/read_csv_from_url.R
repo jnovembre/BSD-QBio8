e3c6406502c5ab3154d946cd3f2dc1aac793fb9c
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-11 06:54:36 -0500

    Updated basic computing 2 files.

--- tutorials/basic_computing_2/snippets/read_csv_from_url.R
+++ tutorials/basic_computing_2/snippets/read_csv_from_url.R
@@ -2,7 +2,7 @@
 # data frame.
 storm_event_data_url <-
   paste("https://www.ncei.noaa.gov/pub/data/swdi/stormevents/csvfiles",
-        "StormEvents_details-ftp_v1.0_d1990_c20210803.csv.gz", sep = "/")
+        "StormEvents_details-ftp_v1.0_d1990_c20220425.csv.gz", sep = "/")
 filename <- tempfile()
 download.file(storm_event_data_url, filename)
 storm_event_data <- read.csv(filename, stringsAsFactors = FALSE)
